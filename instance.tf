253bc7237acdec5e567defd653a2d32e1192678c
Pablo CE <pablocarrera@cye-atlantis.local>
2022-07-16 19:57:34 -0500

    docs: 📜 instance.jf definitions

--- instance.tf
+++ instance.tf
@@ -3,9 +3,11 @@ resource "aws_instance" "crowdsec_instance" {
     count = var.number_of_instances
     subnet_id = var.subnet_id
     instance_type = var.instance_type
-    key_name = "${var.instance_name}"
+    key_name = "${var.instance_name}${index}"
     vpc_security_group_ids = var.security-group-ids
     associate_public_ip_address = true
+    # Add the config you want to set based on cloud-init on user-data folder
+    user_data   = file("${path.module}/user-data/cloud-init.yml")
     tags = {
         Name = var.instance_name
     }
