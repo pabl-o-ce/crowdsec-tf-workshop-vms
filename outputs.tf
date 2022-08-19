999c0c7debaa21081e01ef989331e00b84e98104
pabl-o-ce <cye@poscye.com>
2022-08-19 10:24:40 -0500

    fix: 🐛 fix output format

--- outputs.tf
+++ outputs.tf
@@ -3,14 +3,14 @@ output "crowdsec_workshop" {
   description                 = "CrowdSec workshop - Cloudflare/TransIP"
   value                       = (var.mio_dns_provider == "cf") ? flatten([
                                   [
-                                    for key, value in cloudflare_record.attacker_records : [
-                                      for dkey, dvalue in cloudflare_record.defender_records : { defender_public_ip: dvalue.value, attacker_dns: value.hostname, attacker_public_ip: value.value, defender_dns: dvalue.hostname } 
+                                    for dkey, dvalue in cloudflare_record.defender_records : [
+                                      for key, value in cloudflare_record.attacker_records : { adefender_public_ip: dvalue.value, attacker_dns: value.hostname, attacker_public_ip: value.value, defender_dns: dvalue.hostname } 
                                     ]
                                   ],
                                 ]) : (var.mio_dns_provider == "tsp") ? flatten([
                                   [
-                                    for key, value in transip_vps.crowdsec_instance_attacker : [
-                                      for dkey, dvalue in transip_vps.crowdsec_instance_defender : { defender_public_ip: dvalue.ip_address, attacker_dns: "${value.description}.${data.transip_domain.crowdsec_domain.id}", attacker_public_ip: value.ip_address, defender_dns: "${dvalue.description}.${data.transip_domain.crowdsec_domain.id}" } 
+                                    for dkey, dvalue in transip_vps.crowdsec_instance_defender : [
+                                      for key, value in transip_vps.crowdsec_instance_attacker : { adefender_public_ip: dvalue.ip_address, attacker_dns: "${value.description}.${data.transip_domain.crowdsec_domain.id}", attacker_public_ip: value.ip_address, defender_dns: "${dvalue.description}.${data.transip_domain.crowdsec_domain.id}" } 
                                     ]
                                   ],
                                 ]) : []
