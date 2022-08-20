ca85ea0e0d2f27ae20808254fffa072ca6b18727
pabl-o-ce <cye@poscye.com>
2022-08-19 21:52:36 -0500

    fix: 🐛 fix output as Klau required

--- outputs.tf
+++ outputs.tf
@@ -4,14 +4,16 @@ output "crowdsec_workshop" {
   value                       = (var.mio_dns_provider == "cf") ? flatten([
                                   [
                                     for dkey, dvalue in cloudflare_record.defender_records : [
-                                      for key, value in cloudflare_record.attacker_records : { adefender_public_ip: dvalue.value, attacker_dns: value.hostname, attacker_public_ip: value.value, defender_dns: dvalue.hostname } 
+                                      for key, value in cloudflare_record.attacker_records : { adefender_public_ip: dvalue.value, attacker_dns: value.hostname, attacker_public_ip: value.value } 
                                     ]
                                   ],
                                 ]) : (var.mio_dns_provider == "tsp") ? flatten([
                                   [
                                     for dkey, dvalue in transip_vps.crowdsec_instance_defender : [
-                                      for key, value in transip_vps.crowdsec_instance_attacker : { adefender_public_ip: dvalue.ip_address, attacker_dns: "${value.description}.${data.transip_domain.crowdsec_domain.id}", attacker_public_ip: value.ip_address, defender_dns: "${dvalue.description}.${data.transip_domain.crowdsec_domain.id}" } 
+                                      for key, value in transip_vps.crowdsec_instance_attacker : { adefender_public_ip: dvalue.ip_address, attacker_dns: "${value.description}.${data.transip_domain.crowdsec_domain.id}", attacker_public_ip: value.ip_address } 
                                     ]
                                   ],
                                 ]) : []
 }
+// defender_dns: dvalue.hostname
+// defender_dns: "${dvalue.description}.${data.transip_domain.crowdsec_domain.id}"
